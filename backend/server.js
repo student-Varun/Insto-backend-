5765c71992a8939244e0a4dfc0cd6b4dc4d46959
Varun Trikha <143183439+student-Varun@users.noreply.github.com>
2025-06-24 18:06:23 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ connectDb();
 
 // CORS Configuration
 app.use(cors({
-  origin: 'https://insto-frontend-gsbr.vercel.app',
+  origin: 'http://localhost:5173',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   credentials: true,
 }));
